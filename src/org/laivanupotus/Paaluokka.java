2877ceeb5374e1f3d06ca38a2f039ba752750d63
Elias Kinnunen <evkinn@utu.fi>
2019-03-09 23:48:44 +0200

    Tekoälyä ja pelin toimivuutta.

--- src/org/laivanupotus/Paaluokka.java
+++ src/org/laivanupotus/Paaluokka.java
@@ -3,6 +3,7 @@ package org.laivanupotus;
 import org.laivanupotus.logiikka.Laiva;
 import org.laivanupotus.logiikka.Lauta;
 import org.laivanupotus.pelaaja.Ihmispelaaja;
+import org.laivanupotus.pelaaja.tekoaly.Tekoaly;
 
 public class Paaluokka {
 	
@@ -11,16 +12,26 @@ public class Paaluokka {
 	public static void main(String[] args) {
 		//luodaan lauta
 		Lauta lauta = new Lauta();
+		
+		//luodaan tekoälyn lauta. tätä ei sitten printata, muuten voi mennä pelaaminen liian helpoksi.
+		Lauta tekoLauta = new Lauta();
+		
 		//luodaan pelaaajat
 		Ihmispelaaja ihmispelaaja = new Ihmispelaaja("Testipelaaja");
+		Tekoaly tekoaly = new Tekoaly("Tekoaly");
 		
-		//asetetaan laivat
-		int[] laivapituudet = new int[] {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
-		for (int a : laivapituudet) {
-			int[][] syote = ihmispelaaja.otaSyote(a);
-			Laiva laiva = new Laiva("Lentotukialus", 4, syote[0], syote[1]);
+		//asetetaan laivat - pelaaja
+		int[] laivaPituudet = new int[] {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+		String[] laivanNimet = new String[] {"Lentotukialus", "Risteilijä", "Risteilijä", "Hävittäjä", "Hävittäjä", "Hävittäjä", "Sukellusvene", "Sukellusvene", "Sukellusvene", "Sukellusvene"};
+		for (int i=0; i<laivaPituudet.length; i++) {
+			int[][] syote = ihmispelaaja.otaSyote(laivaPituudet[i], laivanNimet[i]);
+			Laiva laiva = new Laiva(laivanNimet[i], laivaPituudet[i], syote[0], syote[1]);
 			lauta.asetaLaivaLaudalle(laiva);
 		}
+		//asetetaan laivat - tekoäly
+		tekoaly.arvoLaivat(tekoLauta);
+		
+		//tulostetaan VAIN pelaajan lauta
 		lauta.tulostaLauta();
 		
 	}
