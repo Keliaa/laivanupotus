514b1f30c79fadd945f7bb894e61ba59115f46ec
Elias Kinnunen <evkinn@utu.fi>
2019-03-09 23:31:58 +0200

    Jaettiin pelaaja tekoälyyn ja ihmispelaajaan

---
+++ src/org/laivanupotus/pelaaja/tekoaly/Tekoaly.java
@@ -0,0 +1,13 @@
+package org.laivanupotus.pelaaja.tekoaly;
+
+import org.laivanupotus.pelaaja.Pelaaja;
+
+public class Tekoaly extends Pelaaja {
+
+	public Tekoaly(String nimi) {
+		super(nimi);
+	}
+	
+	//tänne sitä tekoälyn toiminnallisuutta
+
+}
