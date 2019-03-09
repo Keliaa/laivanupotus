2cb0e45f4a3063bc89f8dc84acb548919eb3d560
Elias Kinnunen <evkinn@utu.fi>
2019-03-09 23:31:31 +0200

    jaettiin pelaaja tekoälyyn ja ihmispelaajaan

--- src/org/laivanupotus/Paaluokka.java
+++ src/org/laivanupotus/Paaluokka.java
@@ -2,7 +2,7 @@ package org.laivanupotus;
 
 import org.laivanupotus.logiikka.Laiva;
 import org.laivanupotus.logiikka.Lauta;
-import org.laivanupotus.pelaaja.Pelaaja;
+import org.laivanupotus.pelaaja.Ihmispelaaja;
 
 public class Paaluokka {
 	
@@ -12,12 +12,12 @@ public class Paaluokka {
 		//luodaan lauta
 		Lauta lauta = new Lauta();
 		//luodaan pelaaajat
-		Pelaaja pelaaja = new Pelaaja("Testipelaaja");
+		Ihmispelaaja ihmispelaaja = new Ihmispelaaja("Testipelaaja");
 		
 		//asetetaan laivat
 		int[] laivapituudet = new int[] {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
 		for (int a : laivapituudet) {
-			int[][] syote = pelaaja.otaSyote(a);
+			int[][] syote = ihmispelaaja.otaSyote(a);
 			Laiva laiva = new Laiva("Lentotukialus", 4, syote[0], syote[1]);
 			lauta.asetaLaivaLaudalle(laiva);
 		}
--- src/org/laivanupotus/pelaaja/Pelaaja.java
+++ src/org/laivanupotus/pelaaja/Pelaaja.java
@@ -1,10 +1,5 @@
 package org.laivanupotus.pelaaja;
 
-import java.util.Scanner;
-
-import org.laivanupotus.apuluokat.SyoteApu;
-import org.laivanupotus.logiikka.Lauta;
-
 public class Pelaaja {
 	
 	private String nimi;
@@ -15,39 +10,6 @@ public class Pelaaja {
 	
 	public String annaNimi() {
 		return nimi;
-	}
-
-	//Pyytää käyttäjää asettamaan x pituisen laivan laudalle.
-	//Tarkistaa onko syöttämät ruudut oikeassa muodossa ja muuntaa ne numerokoordinaateiksi
-	//Palauttaa int[][] koordinaatit 
-	public int[][] otaSyote(int laivanPituus) {
-
-		String aloitusruutu = "";
-		String lopetusruutu = "";
-		
-		while(true) {
-			Scanner scanner = new Scanner(System.in);
-			
-			System.out.println("Laivan pituus: " + laivanPituus);
-			System.out.println("Anna aloitusruutu");
-			aloitusruutu = scanner.nextLine();
-			System.out.println("Anna lopetusruutu");
-			lopetusruutu = scanner.nextLine();
-			
-			if (SyoteApu.tarkistaSyote(aloitusruutu) && SyoteApu.tarkistaSyote(lopetusruutu)) break;
-				
-			else {
-				System.out.println("Väärän tyyppinen syöte. Anna ruudut muodossa 'A1'");
-				System.out.println();
-				
-				//Tarkasta pituus?
-			}
-				
-		}
-		
-		int[] alkuKoordinaatti = SyoteApu.muunnaKoordinaateiksi(aloitusruutu);
-		int[] loppuKoordinaatti = SyoteApu.muunnaKoordinaateiksi(lopetusruutu);
-		return new int[][] {alkuKoordinaatti, loppuKoordinaatti};
 	}
 	
 }
